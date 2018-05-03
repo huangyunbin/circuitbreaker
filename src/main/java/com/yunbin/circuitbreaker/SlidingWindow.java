839c7a6b45915166aaedeaaa4c94760ebe256e1d
cloud.huang <cloud.huang@vipshop.com>
2018-05-03 23:03:36 +0800

    通过测试

--- src/main/java/com/yunbin/circuitbreaker/SlidingWindow.java
+++ src/main/java/com/yunbin/circuitbreaker/SlidingWindow.java
@@ -4,21 +4,39 @@ package com.yunbin.circuitbreaker;
  * Created by cloud.huang on 18/5/3.
  */
 public class SlidingWindow {
-    private int count = 0;
+    int size;
+    int[] counts;
+    long lastTime = 0;
     
-    public SlidingWindow(int l) {
+    public SlidingWindow(int size) {
+        this.size = size;
+        counts = new int[size];
     }
     
-    public void add(long l) {
-        count++;
+    public void add(long time) {
+        clear(time);
+        int index = (int) (time / size);
+        counts[index] = counts[index] + 1;
+        
+        lastTime = time;
     }
     
-    public int count(long l) {
-        return count;
+    public int count(long time) {
+        clear(time);
+        int result = 0;
+        for (int count : counts) {
+            result += count;
+        }
+        return result;
     }
     
-    public int getCount() {
-        return count;
+    private void clear(long time) {
+        if (time < lastTime + size) {
+            return;
+        }
+        for (int i = 0; i < size; i++) {
+            counts[i] = 0;
+        }
     }
     
     
