f2d381d9c4a9aaa228bfe125962a0486c07498c8
cloud.huang <cloud.huang@vipshop.com>
2018-05-03 22:34:16 +0800

    测试通过

--- src/main/java/com/yunbin/circuitbreaker/SlidingWindow.java
+++ src/main/java/com/yunbin/circuitbreaker/SlidingWindow.java
@@ -4,13 +4,22 @@ package com.yunbin.circuitbreaker;
  * Created by cloud.huang on 18/5/3.
  */
 public class SlidingWindow {
+    private int count = 0;
+    
     public SlidingWindow(int l) {
     }
     
     public void add(long l) {
+        count++;
     }
     
     public int count(long l) {
-        return 1;
+        return count;
     }
+    
+    public int getCount() {
+        return count;
+    }
+    
+    
 }
