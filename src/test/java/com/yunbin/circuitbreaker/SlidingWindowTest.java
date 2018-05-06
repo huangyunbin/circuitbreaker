a3cf85de0759d45fbae8a3d882f098480cd2d154
cloud.huang <cloud.huang@vipshop.com>
2018-05-06 09:13:33 +0800

    跨周期测试通过

--- src/test/java/com/yunbin/circuitbreaker/SlidingWindowTest.java
+++ src/test/java/com/yunbin/circuitbreaker/SlidingWindowTest.java
@@ -106,5 +106,95 @@ public class SlidingWindowTest {
         assertThat(count).isEqualTo(circle * threadNum * num);
     }
     
+    /**
+     * 跨秒的测试
+     */
+    @Test
+    public void addTest7() {
+        int threadNum = 100;
+        final int num = 10000;
+        final int circle = 3;
+        final SlidingWindow slidingWindow = new SlidingWindow(10);
+        final CyclicBarrier barrier = new CyclicBarrier(threadNum);
+        final CountDownLatch countDownLatch = new CountDownLatch(threadNum);
+        
+        for (int i = 0; i < threadNum; i++) {
+            new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        barrier.await();
+                        
+                        for (int i = 0; i < circle; i++) {
+                            for (int j = 0; j < num; j++) {
+                                slidingWindow.add();
+                            }
+                            
+                            TimeUnit.MILLISECONDS.sleep(1100);
+                        }
+                        
+                        countDownLatch.countDown();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }.start();
+            
+        }
+        try {
+            countDownLatch.await();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        int count = slidingWindow.count();
+        assertThat(count).isEqualTo(circle * threadNum * num);
+    }
+    
+    
+    /**
+     * 跨周期的测试
+     */
+    @Test
+    public void addTest8() {
+        int threadNum = 100;
+        final int num = 10000;
+        final int circle = 3;
+        final int size = 2;
+        final SlidingWindow slidingWindow = new SlidingWindow(size);
+        final CyclicBarrier barrier = new CyclicBarrier(threadNum);
+        final CountDownLatch countDownLatch = new CountDownLatch(threadNum);
+        
+        for (int i = 0; i < threadNum; i++) {
+            new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        barrier.await();
+                        
+                        for (int i = 0; i < circle; i++) {
+                            for (int j = 0; j < num; j++) {
+                                slidingWindow.add();
+                            }
+                            
+                            TimeUnit.MILLISECONDS.sleep(1000);
+                        }
+                        
+                        countDownLatch.countDown();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }.start();
+            
+        }
+        try {
+            countDownLatch.await();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        int count = slidingWindow.count();
+        assertThat(count).isEqualTo(size * threadNum * num);
+    }
+    
     
 }
